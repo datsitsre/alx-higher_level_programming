b08bb03c41281a093417c01676bb2f0584cacb50
datsitsre <atsitsredaniel@gmail.com>
2023-03-08 22:47:26 +0000

    3. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for num in range(97, 123):
-    if num == 113 or num == 101:
-        continue
-    else:
-        print("{:c}".format(num), end="")
+    print("{:c}".format(num), end="")
