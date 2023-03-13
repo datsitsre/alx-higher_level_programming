000c14bcab8c516455b15e310f08739bde2af50b
datsitsre <atsitsredaniel@gmail.com>
2023-03-13 23:38:51 +0000

    3. Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,10 +3,7 @@ if __name__ == "__main__":
     import sys
 
     answer = 0
-    if len(sys.argv) - 1 == 0:
-        print("{} arguments.".format(0))
-    else:
-        n = len(sys.argv) - 1
-        for i in range(n):
-            answer += int(sys.argv[i + 1])
-        print("{}".format(answer))
+    n = len(sys.argv) - 1
+    for i in range(n):
+        answer += int(sys.argv[i + 1])
+    print("{}".format(answer))
