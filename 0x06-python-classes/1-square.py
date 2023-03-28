be58bb56a5dc5f4fb7379bcadbc156c5e8d432e6
datsitsre <atsitsredaniel@gmail.com>
2023-03-28 23:03:34 +0000

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+'''
+ Square create and empty square
+
+ Attributes:
+     size (int): Description of size
+ Returns:
+    Nothing
+'''
+
+
+class Square:
+    '''
+        Sqaure class
+        Atrributes:
+            size (int)
+    '''
+    def __init__(self, size):
+        self.__size = size
