c1d7ff498a1e73a68393d2497d8b6519c7b95dd2
datsitsre <atsitsredaniel@gmail.com>
2023-04-12 14:33:17 +0000

    1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 ''' create an inheritance '''
 
+
 class MyList(list):
     ''' create an inheritance list '''
-   
+
     def __init__(self):
+        ''' the instance of the main class '''
         list.__init__(self)
 
-
     def print_sorted(self):
+        ''' the sorted list '''
         print(sorted(self))
