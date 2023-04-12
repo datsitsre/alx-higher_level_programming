aab790b82846e7ff8cc895e3b337dc176e8f8809
datsitsre <atsitsredaniel@gmail.com>
2023-04-12 13:28:33 +0000

    0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+    filename :0-lookup.py
+
+    function :
+        lookup(obj)
+    print a list of avaible attri and method
+'''
+
+
+def lookup(obj):
+    '''
+        print attributes and methods
+    '''
+    print(dir(obj))
