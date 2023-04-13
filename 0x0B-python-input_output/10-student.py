8e6737ea4a5ecc8eee1fe713845fac186449e5a5
datsitsre <atsitsredaniel@gmail.com>
2023-04-13 16:08:31 +0000

    10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Students to json """
+
+
+class Student:
+    """
+        Name : Student
+        Method :
+        Atrributes : first_name, last_name, age
+    """
+    def __init__(self, first_name, last_name, age):
+        """ instance the class """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ to json class """
+        object_type = self.__dict__
+        if type(attrs) is list:
+            for element in attrs:
+                if type(element) is not str:
+                    return self.__dict__
+            dictionary = {}
+            for len_attrub in range(len(attrs)):
+                for data_dic in object_type:
+                    if attrs[len_attrub] == data_dic:
+                        dictionary[data_dic] = object_type[data_dic]
+
+            return dictionary
+
+        return self.__dict__
