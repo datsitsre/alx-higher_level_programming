a6fb8f1f65bb26cad38c0fd5a5a08cfbd71f6bef
datsitsre <atsitsredaniel@gmail.com>
2023-04-13 02:48:09 +0000

    12. My integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,13 +3,16 @@
 
 
 class MyInt(int):
-    ''' Myint rebell class '''
+    '''
+	    Name : Myint rebell class
+        Method : ne, eq
+	'''
 
-    def __ne__self(self, value):
+    def __ne__(self, value):
         ''' Not equal operator  to equal operator'''
         return self.real == value
 
 
-    def __eq__self(self, value):
+    def __eq__(self, value):
         '''  Equal operator  to no equal operator'''
         return self.real != value
