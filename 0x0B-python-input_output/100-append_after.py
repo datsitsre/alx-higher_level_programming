e5a8d304cd03236d86c0cd449ba6636e14dd684e
datsitsre <atsitsredaniel@gmail.com>
2023-04-14 05:36:03 +0000

    13. Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" insert into a file """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ Append to a file """
+    empty_string = ""
+    with open(filename, encoding="utf-8") as file_text:
+        for line in file_text:
+            empty_string += line
+            if search_string in line:
+                empty_string +=  search_string
+    with open(filename, "w") as file_write:
+        file_write.write(empty_string)
+
+
+
+    
+
+
