79cc798b9434016e2655d6837c3e46497f08e832
datsitsre <atsitsredaniel@gmail.com>
2023-04-19 21:02:39 +0000

    0x0C. Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -46,7 +46,7 @@ class Rectangle(Base):
         """Setter value"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be > 0.")
         self.__width = value
 
@@ -55,7 +55,7 @@ class Rectangle(Base):
         """Setter value"""
         if not isinstance(value, int):
             raise TypeError("height be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be > 0.")
         self.__height = value
 
