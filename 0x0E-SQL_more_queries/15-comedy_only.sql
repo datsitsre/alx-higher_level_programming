cffe2c83ef99a9ace5459c660d8afc4d402c7cfa
vagrant <datsitsre>
2023-05-18 13:44:41 +0000

    15. Only Comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,2 +1,4 @@
 -- 15. Only Comedy
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT title FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id 
+WHERE tv_genres.name = "Comedy" GROUP BY title ORDER BY title ASC;
