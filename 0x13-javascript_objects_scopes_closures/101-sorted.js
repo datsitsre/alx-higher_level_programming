c93988456a1d26e4788ee9f1cf1022c821d4ed57
vagrant <datsitsre>
2023-06-13 13:58:39 +0000

    12. Sorted occurences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/**
+ * 12. Sorted occurences
+ */
+
+const dict = require('./101-data').dict;
+const newDict = {};
+
+Object.keys(dict).map(function (key) {
+  if (!Array.isArray(newDict[dict[key]])) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+});
+
+console.log(newDict);
