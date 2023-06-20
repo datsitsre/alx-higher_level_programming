d48b2864a51f4a86b32d7e3c0bc409c91b0286c9
vagrant <datsitsre>
2023-06-20 21:23:22 +0000

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ = '__main__':
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     connet = db.cursor()
-    connet.execute("SELECT * FROM states ORDER BY id ASC")
+    connet.execute("SELECT * FROM states")
     row_fetch = connet.fetchall()
     for row in row_fetch:
         print(row)
