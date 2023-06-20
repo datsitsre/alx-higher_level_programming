eacd4faea12b7c5c23f02360c63d407f892af29d
vagrant <datsitsre>
2023-06-20 21:25:47 +0000

    0. Get all statesus

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-
 """
-List states from the database
+This script lists all states 
 """
 
 import MySQLdb
-import argv
+from sys import argv
 
-if __name__ = '__main__':
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    connet = db.cursor()
-    connet.execute("SELECT * FROM states")
-    row_fetch = connet.fetchall()
-    for row in row_fetch:
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+
+    for row in rows:
         print(row)
-    connet.close()
-    db.close()
