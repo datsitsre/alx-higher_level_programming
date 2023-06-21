712d01302f9b24faaf8cca9574cc59e35a83a6c0
vagrant <datsitsre>
2023-06-21 15:12:51 +0000

    8. First state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+List first State class
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Script for accessing data
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session_instance = session.query(State).first()
+    if session_instance is None:
+        print("Nothing")
+    else:
+        print(session_instance.id, session_instance.name, sep=": ")
