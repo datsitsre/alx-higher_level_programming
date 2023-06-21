9acd5b275a9a7459c2578d3dc096b68345ad7c55
vagrant <datsitsre>
2023-06-21 10:22:40 +0000

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,6 +8,10 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
     db_connect = MySQLdb.connect(host="localhost", user=argv[1], db=argv[2]
                                  passwd=argv[3], port=3306)
 
