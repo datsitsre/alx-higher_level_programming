a83dcda55aa24c1cab2bac7e445f9f7494d3deae
vagrant <datsitsre>
2023-06-21 15:37:28 +0000

    11. Add a new state

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+List first State class
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    """
+    Script for accessing data
+    """
+    uri_db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(uri_db)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_loca = State(name='Louisiana')
+    session.add(state_loca)
+    session.commit()
+    print('{0}'.format(state_loca.id))
+    session.close()
