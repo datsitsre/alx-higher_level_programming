bded14e777c6867c6eec56fb1233ce863e715e5c
vagrant <datsitsre>
2023-06-21 09:56:21 +0000

    0. Get all statesususus

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,6 @@
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
@@ -13,5 +12,6 @@ if __name__ == "__main__":
     rows = cur.fetchall()
     for row in rows:
         print(row)
+
     cur.close()
     db.close()
