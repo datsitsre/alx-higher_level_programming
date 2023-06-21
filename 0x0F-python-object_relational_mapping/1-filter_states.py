0077a7d3da4206619c41d20b103829b16a2503ea
vagrant <datsitsre>
2023-06-21 10:10:08 +0000

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" list all states with name starting with N from hbtn_0e_0_usa """
+"""list all states with name starting with N from hbtn_0e_0_usa """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
     """ the mysql code """
-    db_connect = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                                 passwd=argv[2], port=3306)
+    db_connect = MySQLdb.connect(host="localhost", user=argv[1], db=argv[2]
+                                 passwd=argv[3], port=3306)
 
     db_cur = db.cursor()
     db_cur.execute("SELECT * FROM states WHERE name LIKE
