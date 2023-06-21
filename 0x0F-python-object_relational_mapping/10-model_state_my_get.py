ffcd92b199310c01f49b1f4a58dab1b6c6a24c5b
vagrant <datsitsre>
2023-06-21 15:32:37 +0000

    10. Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -12,10 +12,11 @@ if __name__ == '__main__':
     """
     Script for accessing data
     """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+    uri_db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(uri_db)
     Session = sessionmaker(bind=engine)
+
     session = Session()
     instance_sess = session.query(State).filter(State.name == argv[4]).first()
 
