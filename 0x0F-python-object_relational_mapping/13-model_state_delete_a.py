cbd5bb1ad6f2d43fa1a67d6ccf60c466b85ab680
vagrant <datsitsre>
2023-06-21 15:45:10 +0000

    13. Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     session = Session()
 
     for sess_instan in session.query(State).filter(State.name.contains('a')):
-        session.delete(instance)
+        session.delete(sess_instan)
 
     session.commit()
     session.close()
