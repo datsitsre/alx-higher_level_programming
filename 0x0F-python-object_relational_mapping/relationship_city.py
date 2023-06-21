9b027308995cf2737efddd883f92bd984efd627e
vagrant <datsitsre>
2023-06-21 16:00:47 +0000

    15. City relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Define the City class and Base instance of declarative_base()
+"""
+from relationship_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """
+    Class : City
+    Attributes : id , name, state_id
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
