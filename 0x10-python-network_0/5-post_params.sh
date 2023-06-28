90841d6defb6a137868b86f796f5ff6a5b938e1a
vagrant <datsitsre>
2023-06-28 17:24:44 +0000

    5. cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # URL send a POST request to the passwd URL and displays
-curl -s -X POST -d "{"email":"test@gmail.com", "subject":"I will always be here for PLD"}" "${1}"
+curl -s -X POST -d '{"email":"test@gmail.com","subject":"I will always be here for PLD"}' "${1}"
